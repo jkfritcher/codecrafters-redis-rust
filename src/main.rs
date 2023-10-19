d3ccafe69a5ef85fd8a770cf0d69084d864f036a
Jason Fritcher <jkf@wolfnet.org>
2023-10-19 14:21:53 -0500

    Add stage 2 response

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{Shutdown, TcpListener, TcpStream}
+};
 
-fn main() {
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    stream.write(b"+PONG\r\n")?;
+    stream.shutdown(Shutdown::Both)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     eprintln!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
